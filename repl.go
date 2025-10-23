b0d9c9a67e40401dc1980907bbbd7d036a24a8de
Aditya Mahapatra <adma@redefine.co>
2025-10-24 04:32:04 +0530

    Prompt exercise.

--- repl.go
+++ repl.go
@@ -1,6 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func PokedexPrompt() {
+	const prompt = "Pokedex > "
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		if scanner.Scan() {
+			input := scanner.Text()
+
+			if len(input) == 0 {
+				continue
+			}
+
+			output := cleanInput(input)
+			fmt.Printf("Your command was: %s\n", output[0])
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := make([]string, 0)
