36d741b344f8a34dfb4065eb5e0c64485e8b5b22
Aditya Mahapatra <adma@redefine.co>
2025-10-27 23:21:56 +0530

    Add LocationAreas function to fetch and return location area names from PokeAPI.

---
+++ internal/pokedexapi/pokedex.go
@@ -0,0 +1,52 @@
+package pokedexapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type LocationArea struct {
+	Count    int     `json:"count"`
+	Next     string  `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type Config struct {
+	Offset int
+	Limit  int
+}
+
+func LocationAreas(conf *Config) []string {
+	var areas []string
+
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/location-area?offset=%d&limit=%d", conf.Offset, conf.Limit)
+	request, err := http.Get(url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer request.Body.Close()
+
+	response, err := io.ReadAll(request.Body)
+	if err != nil {
+		log.Fatalf("Invalid Response: Status Code - %d: %s\n", request.StatusCode, request.Body)
+	}
+
+	var locArea LocationArea
+	if err = json.Unmarshal(response, &locArea); err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, result := range locArea.Results {
+		areas = append(areas, result.Name)
+	}
+
+	return areas
+}
